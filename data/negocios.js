6797a1f31515ade25b1ad0d0369bf0f59c45abc4
akicolombia <akicomerciocolombia@gmail.com>
2025-07-28 14:15:40 -0500

    Mi primer commit

---
+++ data/negocios.js
@@ -0,0 +1,34 @@
+export const negocios = [
+  {
+    id: 1,
+    nombre: "FONDA LA CENAIDA",
+    categoria: "Alimentos",
+    direccion: "Calle Principal 123",
+    telefono: "+1234567890",
+    horario: "6:00 AM - 8:00 PM"
+  },
+  {
+    id: 2,
+    nombre: "JARTADERO LA PANCRACIA",
+    categoria: "Salud",
+    direccion: "Avenida Central 456",
+    telefono: "+0987654321",
+    horario: "8:00 AM - 10:00 PM"
+  },
+  {
+    id: 3,
+    nombre: "CHONGO DONDE ORTE",
+    categoria: "Construcción",
+    direccion: "Calle Secundaria 789",
+    telefono: "+1122334455",
+    horario: "7:30 AM - 7:00 PM"
+  },
+  {
+    id: 4,
+    nombre: "Cafetería Aromas",
+    categoria: "Alimentos",
+    direccion: "Plaza Central 101",
+    telefono: "+5566778899",
+    horario: "7:00 AM - 8:00 PM"
+  }
+];
