b20b2d8ad85a0e9e96bca7402283c3eb1f0f149a
akicolombia <akicomerciocolombia@gmail.com>
2025-07-31 16:18:58 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.aki.comercio',
+  appName: 'aki',
+  webDir: '.next'
+};
+
+export default config;
---
+++ negocios.js
@@ -0,0 +1,7 @@
+export const negocios = [
+  {
+    id: 1,
+    nombre: "Ejemplo Negocio",
+    productos: ["Producto 1", "Producto 2"]
+  }
+];
